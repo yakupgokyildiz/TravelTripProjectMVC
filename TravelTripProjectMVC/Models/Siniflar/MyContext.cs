aaced382bdce3564661de324b96a14f13bc2f93a
ykpgkyldz <ykpgkyldz@gmail.com>
2020-07-07 23:08:33 +0300

    FirstLoad

---
+++ TravelTripProjectMVC/Models/Siniflar/MyContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace TravelTripProjectMVC.Models.Siniflar
+{
+    public class MyContext : DbContext
+    {
+        
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<AdresBlog> AdresBlogs { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<iletisim> iletisims { get; set; }
+        public DbSet<Yorumlar> Yorumlars { get; set; }
+    }
+}
