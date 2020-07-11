86a223485813916c3eac94ee6cf54b05037242a7
ykpgkyldz <ykpgkyldz@gmail.com>
2020-07-12 00:08:36 +0300

    thirdload

--- TravelTripProjectMVC/Controllers/BlogController.cs
+++ TravelTripProjectMVC/Controllers/BlogController.cs
@@ -9,12 +9,23 @@ namespace TravelTripProjectMVC.Controllers
 {
     public class BlogController : Controller
     {
+        BlogYorum blogYorum = new BlogYorum();
         MyContext myContext = new MyContext();
         // GET: Blog
         public ActionResult Index()
         {
-            var degerler = myContext.Blogs.ToList();
-            return View(degerler);
+            //var degerler = myContext.Blogs.ToList();
+            blogYorum.Deger1 = myContext.Blogs.ToList();
+            blogYorum.Deger3 = myContext.Blogs.OrderByDescending(x => x.ID).Take(3).ToList();
+            return View(blogYorum);
+        }
+       
+        public ActionResult BlogDetay(int id)
+        {
+            //var blogbul = myContext.Blogs.Where(i => i.ID == id).ToList();
+            blogYorum.Deger1 = myContext.Blogs.Where(x => x.ID == id).ToList();
+            blogYorum.Deger2 = myContext.Yorumlars.Where(x => x.ID == id).ToList();
+            return View(blogYorum);
         }
     }
 }
