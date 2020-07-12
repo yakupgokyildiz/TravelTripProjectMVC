7595e222b480d71deedfe1a69c20eb670c44fac6
ykpgkyldz <ykpgkyldz@gmail.com>
2020-07-12 15:58:02 +0300

    fourthLoad

--- TravelTripProjectMVC/Controllers/DefaultController.cs
+++ TravelTripProjectMVC/Controllers/DefaultController.cs
@@ -3,20 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProjectMVC.Models.Siniflar;
 
 namespace TravelTripProjectMVC.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        MyContext myContext = new MyContext();
         public ActionResult Index()
         {
-            return View();
+            var degerler = myContext.Blogs.ToList();
+            return View(degerler);
         }
 
         public ActionResult About()
         {
             return View();
+        }
+
+        public PartialViewResult Partial1()
+        {
+            var degerler = myContext.Blogs.OrderByDescending(x => x.ID).Take(2).ToList();
+            return PartialView(degerler);
+        }
+
+        public PartialViewResult Partial2()
+        {
+            var degerler = myContext.Blogs.Where(x => x.ID == 1).ToList();
+            return PartialView(degerler);
         }
     }
 }
