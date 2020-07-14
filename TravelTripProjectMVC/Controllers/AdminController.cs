95b8e5579e5f5187ba2a7694162a1d1dec53a2a8
ykpgkyldz <ykpgkyldz@gmail.com>
2020-07-14 15:56:19 +0300

    sixthLoad

---
+++ TravelTripProjectMVC/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProjectMVC.Models.Siniflar;
+
+namespace TravelTripProjectMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: 
+        MyContext myContext = new MyContext();
+        public ActionResult Index()
+        {
+            var degerler = myContext.Blogs.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniBlog()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniBlog(Blog p)
+        {
+            myContext.Blogs.Add(p);
+            myContext.SaveChanges();
+            return RedirectToAction("Index");
+        } 
+
+        public ActionResult BlogSil(int id)
+        {
+            var deger = myContext.Blogs.Find(id);
+            myContext.Blogs.Remove(deger);
+            myContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
