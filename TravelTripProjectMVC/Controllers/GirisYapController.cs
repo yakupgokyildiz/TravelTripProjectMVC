d22ef072e36962eeddef20faacd8278ef9cd2578
ykpgkyldz <ykpgkyldz@gmail.com>
2020-07-28 00:27:55 +0300

    EighthLoad

--- TravelTripProjectMVC/Controllers/GirisYapController.cs
+++ TravelTripProjectMVC/Controllers/GirisYapController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using TravelTripProjectMVC.Models.Siniflar;
 
 namespace TravelTripProjectMVC.Controllers
 {
     public class GirisYapController : Controller
     {
+        MyContext myContext = new MyContext();
         // GET: GirisYap
         public ActionResult Index()
         {
@@ -16,6 +19,26 @@ namespace TravelTripProjectMVC.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Admin admin)
+        {
+            var bilgiler = myContext.Admins.FirstOrDefault(x => x.Kullanici == admin.Kullanici && x.Sifre == admin.Sifre);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.Kullanici, false);
+                Session["Kullanici"] = bilgiler.Kullanici.ToString();
+                return RedirectToAction("Index", "Admin");
+            }
+            else
+                return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "GirisYap");
         }
     }
 }
